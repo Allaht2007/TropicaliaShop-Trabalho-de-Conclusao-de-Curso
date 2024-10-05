ace1db5f563906d28bba34004cc8f3bf8ccb2b3f
unknown <allanht2007@gmail.com>
2024-10-05 18:21:30 -0300

    Sexto commit

--- models/Control/controleUsers.js
+++ models/Control/controleUsers.js
@@ -20,9 +20,35 @@ router.post("/cadastraUser", async (req, res) => {
     let senhaUser = req.body.senhaUser; 
     let btnTipo  = req.body.btnPfHidden;
 
-    console.log(`nameUser: ${nameUser}, PFpjUser: ${cpf}, emailUser: ${emailUser}, senhaUser: ${senhaUser}, btnTipo: ${btnTipo}`);
+    if (!nameUser || !cpf || !emailUser || !senhaUser || !btnTipo) {
+        console.log('Campos obrigatórios faltando');
+        return res.status(400).send('Todos os campos são obrigatórios.');
+    }
+    try {
+        // Verifica se o usuário já existe pelo email
+        let usuario = await Usuario.findOne({ where: { email_user: emailUser } });
+        if (!usuario) {
+            // Gera um salt e hash para a senha
+            let criahash = bcrypt.genSaltSync(10);
+            let hash = bcrypt.hashSync(senhaUser, criahash);
+            
+            await Usuario.create({
+                nome_user: nameUser,
+                email_user: emailUser,
+                senha_user: hash,
+                cpf_cnpj: cpf,
+                tipo_user: btnTipo
+            });
 
-  
+            res.redirect("/");
+        } else {
+            res.send('Usuário já existe');
+        }
+    } catch (erro) {
+        console.log('Erro ao cadastrar usuário:', erro);
+        res.status(500).send('Erro ao cadastrar usuário');
+    }
+   
 });
 
 module.exports = router;
