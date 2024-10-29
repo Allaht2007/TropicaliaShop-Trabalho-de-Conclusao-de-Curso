59232e76ecb956dc55112b2728368bb8b72cb140
Dafael da silva <djoaovitor968@gmail.com>
2024-10-29 17:50:53 -0300

    vigésima primeira

--- models/Control/ControleFav.js
+++ models/Control/ControleFav.js
@@ -4,5 +4,8 @@ const bodyParser = require("body-parser");
 const Fav = require("../Tables/Fav");
 
 router.use(bodyParser.urlencoded({extended:true}));
-
+router.get("/favoritos",(req,res)=>{
+    res.render("../views/Telas/prodFavoritos.ejs");
+    });
+    
 module.exports = router;
