619cf68578f2abb9935f5e5547aceae3e2add66e
Allan Hoffmann Trombim <allanht2007@gmail.com>
2025-10-27 23:20:04 -0300

    Corrigindo bug Vercel

--- BancoDados/baseDados.js
+++ BancoDados/baseDados.js
@@ -2,14 +2,18 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false 
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false 
+        }
+    },
+    define: {
+        timestamps: false,
+        freezeTableName: true
     }
-  }
 });
 
 module.exports = sequelize;
